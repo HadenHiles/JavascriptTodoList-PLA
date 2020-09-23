cec254d847e88c64bc1769a55537bbaf758ca122
HadenHiles <haden@howtohockey.com>
2020-09-23 12:23:23 -0400

    Effectively pull list items into the dom via express endpoint and local json file

--- public/script.js
+++ public/script.js
@@ -1,34 +1,33 @@
 window.addEventListener('load', function () {
     // Retrieve current items from the list.json
-    var listItems = {};
-    fetch('list.json')
-        .then(response => response.json())
-        .then(jsonResponse => listItems = jsonResponse);
+    var listItems = loadJSON('/list');
 
+    // Create references to the list and item template elements
     var list = document.querySelector('.list');
     var itemTemplate = document.getElementById('item-template');
 
-    if (listItems.items) {
-        // Clear the default text
-        list.innerHTML();
+    // Ensure there are items in the list.json to display
+    if (listItems.items && listItems.items.length > 0) {
+        list.innerHTML = ""; // Clear the default text
 
         // Loop through the list items in the json file
-        listItems.items.forEach(x, item => {
+        listItems.items.forEach((item, x) => {
             // Build the list item from the list template
-            var newItem = itemTemplate.innerHTML;
-            newItem.removeAttribute('id');
+            var newItem = document.createElement('li');
+            newItem.setAttribute('class', 'item');
             newItem.setAttribute('id', `item-${x}`); // give the item a unique id
+            newItem.innerHTML = itemTemplate.innerHTML; // Setup it's html
 
             // Retrieve the child nodes of the new item so we can update the text and checkbox
             var children = newItem.childNodes;
             var checkbox = children[1]; // The checkbox
-            var itemText = children[2]; // The item text
-            
-            itemText.innerHTML = item.title; // Set the todo item text
-            checkbox.setAttribute('checked', item.checked); // Set if the item is checked
+            var itemText = children[3]; // The item text
 
-            // If it is then put a line-through the item text label
+            itemText.innerHTML = item.title; // Set the todo item text
+
+            // If item is checked put a line-through and check the box
             if (item.checked) {
+                checkbox.setAttribute('checked', true); // Set if the item is checked
                 itemText.setAttribute('class', 'strikethrough');
             }
 
@@ -36,3 +35,17 @@ window.addEventListener('load', function () {
         });
     }
 });
+
+// Load json file from express server
+function loadJSON(path) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", path, false);
+    xmlhttp.overrideMimeType('application/json');
+    
+    xmlhttp.send();
+    if(xmlhttp.status == 200) {
+        return JSON.parse(xmlhttp.responseText);
+    } else {
+        return null;
+    }
+}
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var list = require('./public/list.json');
 var app = express();
 
 // view at http://localhost:8080
 app.use(express.static(__dirname + '/public'));
+
+app.get('/list', (req, res) => {
+    console.log(list);
+    res.send(list);
+});
 
 console.log("Todo list running on http://localhost:8080");
 app.listen(8080);
