91b33eb8b151b6d6c4a040abcada5b68f2254936
HadenHiles <haden@howtohockey.com>
2020-09-23 14:49:07 -0400

    Delete items from the list with delete buttons for each item. Toggle the completed state of each item when it's checkbox value is changed

--- public/script.js
+++ public/script.js
@@ -1,51 +1,134 @@
 window.addEventListener('load', function () {
+    /* 
+    * LOAD list.json contents
+    */
     // Retrieve current items from the list.json
-    var listItems = loadJSON('/list');
+    loadJSON('/list', (items) => {
+        var listItems = items; // assign the json response to a variable for looping through
+        var list = document.querySelector('.list'); // Create references to the list
 
-    // Create references to the list and item template elements
-    var list = document.querySelector('.list');
-    var itemTemplate = document.getElementById('item-template');
+        // Ensure there are items in the list.json to display
+        if (listItems.items && listItems.items.length > 0) {
+            list.innerHTML = ""; // Clear the default text
 
-    // Ensure there are items in the list.json to display
-    if (listItems.items && listItems.items.length > 0) {
-        list.innerHTML = ""; // Clear the default text
+            // Loop through the list items in the json file
+            listItems.items.forEach(item => {
+                var existingItem = buildItem(item); // generate item from the template
 
-        // Loop through the list items in the json file
-        listItems.items.forEach((item, x) => {
-            // Build the list item from the list template
-            var newItem = document.createElement('li');
-            newItem.setAttribute('class', 'item');
-            newItem.setAttribute('id', `item-${x}`); // give the item a unique id
-            newItem.innerHTML = itemTemplate.innerHTML; // Setup it's html
+                list.append(existingItem); // append each existing list item to the list
+            });
+        }
+    });
 
-            // Retrieve the child nodes of the new item so we can update the text and checkbox
-            var children = newItem.childNodes;
-            var checkbox = children[1]; // The checkbox
-            var itemText = children[3]; // The item text
+    /*
+    * EVENTS
+    */
+    // Add button
+    document.getElementById('add-item').addEventListener('click', () => {
+        var list = document.querySelector('.list'); // create reference to the list element
+        var itemText = document.getElementById('item-text').value; // get the input text value
 
-            itemText.innerHTML = item.title; // Set the todo item text
-
-            // If item is checked put a line-through and check the box
-            if (item.checked) {
-                checkbox.setAttribute('checked', true); // Set if the item is checked
-                itemText.setAttribute('class', 'strikethrough');
-            }
-
-            list.append(newItem);
+        // call the server and update dom after successful creation in list.json
+        httpGetAsync(`/add?title=${itemText}`, (item) => {
+            var newItem = buildItem(item); // get a new list item from the template
+            list.append(newItem); // append the item to the list in the DOM
+            document.getElementById('item-text').value = ""; // reset the item text input value
         });
-    }
+    });
 });
 
-// Load json file from express server
-function loadJSON(path) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", path, false);
-    xmlhttp.overrideMimeType('application/json');
-    
-    xmlhttp.send();
-    if(xmlhttp.status == 200) {
-        return JSON.parse(xmlhttp.responseText);
-    } else {
-        return null;
+/**
+ * Generate a new list item from the item template html
+ * @param {any} item the item object returned from the list.json file/express server
+ */
+function buildItem(item) {
+    var itemTemplate = document.getElementById('item-template'); // the item template to use
+
+    // Build the list item
+    var newItem = document.createElement('li');
+    newItem.setAttribute('class', 'item'); // make sure it has the right class
+    newItem.setAttribute('id', item.uuid); // give the item a unique id
+    newItem.innerHTML = itemTemplate.innerHTML; // Setup it's html from list template
+
+    var children = newItem.childNodes; // Retrieve the child nodes of the new item so we can update the text and checkbox
+    var checkbox = children[1]; // The checkbox
+    checkbox.setAttribute('onchange', `toggleComplete('${item.uuid}')`);
+    var itemText = children[3]; // The item text
+    var deleteBtn = children[5]; // The delete button
+    deleteBtn.setAttribute(`onclick`, `deleteItem('${item.uuid}')`); // pass the onclick event to the dom to avoid unnecessarily refreshing event listeners every time an item is added
+
+    itemText.innerHTML = item.data.title; // Set the todo item text
+
+    // If item is checked put a line-through and check the box
+    if (item.data.checked) {
+        checkbox.setAttribute('checked', true); // Set if the item is checked
+        itemText.setAttribute('class', 'strikethrough');
     }
+
+    return newItem;
+}
+
+/**
+ * Deletes the item by specified id (uuid)
+ * @param {String} id 
+ */
+function deleteItem(id) {
+    httpGetAsync(`/delete?id=${id}`, (success) => {
+        if (success) {
+            document.getElementById(id).remove();
+        }
+    });
+}
+
+function toggleComplete(id) {
+    // toggle the ui strikethrough regardless of server success
+    var children = document.getElementById(id).childNodes;
+    var checkbox = children[1]; // The checkbox
+    var itemText = children[3]; // The item text
+    if (checkbox.checked) {
+        checkbox.removeAttribute('checked');
+        itemText.removeAttribute('class');
+    } else {
+        checkbox.setAttribute('checked', true);
+        itemText.setAttribute('class', 'strikethrough');
+    }
+
+    httpGetAsync(`/toggle?id=${id}`, (result) => {
+        if (result.success) {
+            // toggle the current checked state depending on success
+            checkbox.setAttribute('checked', result.checked);
+            if (result.checked) {
+                itemText.setAttribute('class', 'strikethrough');
+            } else {
+                itemText.removeAttribute('class');
+            }
+        }
+    });
+}
+
+// Load json file from express server
+function loadJSON(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.status == 200 && xmlHttp.readyState == 4) {
+            callback(JSON.parse(xmlHttp.responseText));
+        }
+    }
+
+    xmlHttp.open("GET", url, true);
+    xmlHttp.overrideMimeType('application/json');
+    xmlHttp.send(null);
+}
+
+// Make an http request
+function httpGetAsync(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.status == 200 && xmlHttp.readyState == 4) {
+            callback(JSON.parse(xmlHttp.responseText));
+        }
+    }
+    xmlHttp.open("GET", url, true); // true = asynchronous 
+    xmlHttp.send(null);
 }
--- server.js
+++ server.js
@@ -1,14 +1,82 @@
 var express = require('express');
+var fs = require('fs');
 var list = require('./public/list.json');
 var app = express();
 
 // view at http://localhost:8080
 app.use(express.static(__dirname + '/public'));
 
+// Return the list json data
 app.get('/list', (req, res) => {
-    console.log(list);
     res.send(list);
+});
+
+// Add item to list with provided title
+app.get('/add', (req, res) => {
+    var title = req.query.title;
+    var uuid = create_UUID();
+
+    var item = {uuid: uuid, data: { "title": title, "checked": false }};
+    console.log(item);
+    list.items.push(item);
+
+    writeList(list);
+
+    res.send(item);
+});
+
+// delete the specified item by uuid
+app.get('/delete', (req, res) => {
+    var id = req.query.id;
+    var success = false;
+    list.items = list.items.filter(item => {
+        success = item.uuid == id;
+        return item.uuid != id;
+    });
+
+    writeList(list);
+    res.send({ "success": success });
+});
+
+// toggle the current checked status
+app.get('/toggle', (req, res) => {
+    var id = req.query.id;
+    var success = false;
+    // filter based on uuid (could return multiple so being careful here)
+    var targetItem = list.items.filter(item => {
+        return item.uuid == id;
+    })[0];
+
+    targetItem.data.checked = !targetItem.data.checked;
+
+    writeList(list);
+    res.send({ "success": targetItem != null, "checked": targetItem.data.checked });
+});
+
+// fix the favicon error in browser console
+app.get('/favicon.ico', (req, res) => {
+    res.sendStatus(200);
 });
 
 console.log("Todo list running on http://localhost:8080");
 app.listen(8080);
+
+// convenience function for writing to the list.json file
+function writeList(list) {
+    fs.writeFile('./public/list.json', JSON.stringify(list), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+// SOURCE: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
